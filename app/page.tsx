620c254ec1efae2e493ae7b0601692fb93b6d41e
bagoesrex <bagoesrex@gmail.com>
2025-07-11 15:23:31 +0700

    style: adjust header background and shadow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="max-w-3xl h-screen mx-auto pt-16">
-      <section className="flex flex-col justify-center items-center h-full">
+    <main className="max-w-3xl h-[150vh] mx-auto mt-1 pt-16">
+      <section className="flex flex-col justify-center items-center rounded-2xl bg-white shadow-md h-full">
         <h1>{` < Hero Section />`}</h1>
       </section>
     </main>
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,8 +2,8 @@ import Navbar from "./navbar";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 w-full z-50  h-16 px-5">
-            <div className="flex flex-row justify-around items-center backdrop-blur-md max-w-3xl mx-auto h-full border-b-1 border-black">
+        <header className="fixed top-0 w-full z-50 h-14 mt-1">
+            <div className="flex flex-row justify-between bg-white rounded-2xl items-center shadow-md max-w-3xl mx-auto px-5 h-full">
                 <div>
                     <h1>{`< Header Icon />`}</h1>
                 </div>
