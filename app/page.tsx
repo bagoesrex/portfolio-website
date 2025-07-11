096d5c6713aff5370f494a0359645ba0ebf911a4
bagoesrex <bagoesrex@gmail.com>
2025-07-11 20:28:01 +0700

    feat(home): add initial hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="max-w-3xl h-[150vh] mx-auto mt-1 pt-16">
-      <section className="flex flex-col justify-center items-center rounded-2xl bg-white shadow-md h-full">
-        <h1>{` < Hero Section />`}</h1>
+      <section className="flex flex-col gap-5 px-5 items-center rounded-2xl border-1 border-black bg-white shadow-md h-full">
+        <div className="flex flex-row pt-15 gap-4">
+          <div className="flex flex-col space-y-2">
+            <h1 className="text-3xl font-extrabold bg-gradient-to-r from-blue-500 to-sky-400 text-transparent bg-clip-text">
+              Hi, I'm Bagus
+            </h1>
+            <h2 className="text-xl font-semibold text-blue-600">Fullstack Developer</h2>
+            <p className="text-gray-700 leading-relaxed">
+              I'm a tech enthusiast with a strong interest in building responsive and interactive web applications.
+              I enjoy experimenting with new technologies and creating clean user interfaces.
+            </p>
+          </div>
+          <div className="w-full overflow-hidden rounded-xl shadow-md size-50">
+            <Image
+              src="/images/megumine.webp"
+              width={400}
+              height={400}
+              alt="Ilustrasi karakter Megumine"
+              className="w-full h-auto object-cover object-bottom"
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 export default function Header() {
     return (
         <header className="fixed top-0 w-full z-50 h-14 mt-1">
-            <div className="flex flex-row justify-between bg-white rounded-2xl items-center shadow-md max-w-3xl mx-auto px-5 h-full">
+            <div className="flex flex-row justify-between bg-white border-1 border-black rounded-2xl items-center shadow-md max-w-3xl mx-auto px-5 h-full">
                 <div>
                     <h1>{`< Header Icon />`}</h1>
                 </div>
