05d26fa9cd9c9d0488157af1471c48b0a6ac4972
bagoesrex <bagoesrex@gmail.com>
2025-07-16 16:52:03 +0700

    feat(about): add about page with intro section component

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import IntroSection from "@/components/about/intro-section";
+
+export default function AboutPage() {
+    return (
+        <main className="max-w-3xl mx-auto mt-1 pt-23 flex flex-col gap-10 pb-10 *:mx-5 text-gray-700">
+
+            <IntroSection />
+
+        </main>
+    )
+}
---
+++ components/about/intro-section.tsx
@@ -0,0 +1,21 @@
+export default function IntroSection() {
+    return (
+        <section id="intro" className="flex flex-col gap-5">
+            <div className="flex flex-row gap-4">
+                <div className="flex flex-col flex-3/4 gap-3">
+                    <h1 className="text-2xl font-semibold font-sans text-gray-950 bg-clip-text">
+                        Hey, I’m Bagus Rahmawan from Semarang. I love building, breaking, and fixing the web. one line of code at a time.
+                    </h1>
+                    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cumque itaque. Modi tenetur molestiae veniam totam quia, sit vero! Quae sapiente dolore blanditiis aspernatur sed aut debitis quos delectus omnis!
+                        Nisi, reprehenderit tempora laborum provident consequuntur commodi quos placeat deleniti sit, debitis vitae nostrum dicta similique! Repellendus ad explicabo iste <br /> sit facere iure amet suscipit laboriosam. Eos autem asperiores labore.
+                        Inventore, fugit odit deleniti ratione, quasi voluptatum fuga neque amet eligendi ullam est suscipit repellat non aspernatur nam, sed repudiandae! Ut autem cum est, odio natus porro fugiat omnis iusto.</p>
+                </div>
+                <div className="flex flex-col flex-1/4">
+                    <h1 className="text-3xl font-extrabold bg-gradient-to-r from-blue-500 to-sky-400 text-transparent bg-clip-text">
+                        Todo Image
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
+}
