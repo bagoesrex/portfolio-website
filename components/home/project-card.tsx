ff9e50c30d00a596f19374c6afc4b4a9048c9c26
bagoesrex <bagoesrex@gmail.com>
2025-07-24 00:13:35 +0700

    feat(home): integrate Sanity CMS for project section

--- components/home/project-card.tsx
+++ components/home/project-card.tsx
@@ -1,23 +1,29 @@
-import { Project } from "@/types/project";
 import { ChevronRight } from "lucide-react";
+import { PortableText } from "next-sanity";
 import Link from "next/link";
 
 interface ProjectCardProps {
-    project: Project;
-};
+    slug: string;
+    title: string;
+    date: string;
+    body: Array<any>;
+    techStack: Array<string>;
+}
 
-export default function ProjectCard({ project }: ProjectCardProps) {
+export default function ProjectCard({ slug, title, date, body, techStack }: ProjectCardProps) {
     return (
         <div className="flex flex-col gap-2 border-b-2 border-primary/80 pb-3">
             <div className="flex flex-row justify-between">
-                <h3 className="font-bold text-secondary">{project.title}</h3>
-                <Link href={`/projects/${project.slug}`}>
+                <h3 className="font-bold text-secondary">{title}</h3>
+                <Link href={`/projects/${slug}`}>
                     <ChevronRight className="cursor-pointer hover:opacity-70 text-secondary" />
                 </Link>
             </div>
-            <p className="text-sm">{project.description}</p>
+            <div className="text-sm mt-1 line-clamp-3">
+                {Array.isArray(body) && <PortableText value={body} />}
+            </div>
             <div className="flex flex-wrap gap-2">
-                {project.techStack.map((tech) => (
+                {techStack.map((tech) => (
                     <span key={tech} className="bg-gray-200/40 text-xs rounded px-2 py-1">
                         {tech}
                     </span>
--- components/home/projects-section.tsx
+++ components/home/projects-section.tsx
@@ -1,16 +1,33 @@
-import { dummyProjects } from "@/data/dummyProjects";
 import CustomButton from "../shared/custom-button";
 import ProjectCard from "./project-card";
+import { type SanityDocument } from "next-sanity";
+import { client } from "@/sanity/client";
 
-export default function ProjectsSection() {
+const PROJECTS_QUERY = `*[
+    _type == "project"
+    && defined(slug.current)
+  ]|order(publishedAt desc)[0...3]{_id, title, slug, publishedAt, body, techStack}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function ProjectsSection() {
+    const projects = await client.fetch<SanityDocument[]>(PROJECTS_QUERY, {}, options);
+
     return (
         <section id="projects">
             <div className="flex flex-col gap-4">
                 <h2 className="text-2xl font-extrabold text-primary">
                     Featured Project
                 </h2>
-                {dummyProjects.map((project) => (
-                    <ProjectCard key={project.id} project={project} />
+                {projects.map((project) => (
+                    <ProjectCard
+                        key={project._id}
+                        title={project.title}
+                        slug={project.slug.current}
+                        date={project.publishedAt}
+                        body={project.body}
+                        techStack={project.techStack}
+                    />
                 ))}
                 <CustomButton link="/projects">See All</CustomButton>
             </div>
