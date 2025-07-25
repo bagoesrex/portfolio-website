801ae3c87e3f988fc631713a219ba954877e637b
bagoesrex <bagoesrex@gmail.com>
2025-07-25 21:15:36 +0700

    feat(hero): implement image sections using AspectRatio

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,29 +1,36 @@
 import Image from "next/image";
+import { AspectRatio } from "../ui/aspect-ratio";
 
 export default function HeroSection() {
     return (
-        <section id="hero" className="flex flex-col gap-5 items-center">
-            <div className="flex flex-row gap-4">
-                <div className="flex flex-col space-y-2">
+        <section id="hero" className="flex flex-col items-center">
+            <div className="flex flex-col-reverse md:flex-row items-start md:items-center gap-6 max-w-4xl w-full">
+                <div className="flex-1 text-left">
                     <h1 className="text-3xl font-extrabold text-primary">
                         Hi, I'm Bagus
                     </h1>
-                    <h2 className="text-xl font-semibold text-primary">Fullstack Developer</h2>
+                    <h2 className="text-xl font-semibold text-primary">
+                        Fullstack Developer
+                    </h2>
                     <p className="leading-relaxed">
-                        I'm a tech enthusiast with a strong interest in building responsive and interactive web applications.
-                        I enjoy experimenting with new technologies and creating clean user interfaces.
+                        I'm a tech enthusiast with a strong interest in building responsive and interactive web applications. I enjoy experimenting with new technologies and creating clean user interfaces.
                     </p>
                 </div>
-                <div className="w-full overflow-hidden rounded-xl shadow-md size-50">
-                    <Image
-                        src="/images/megumine.webp"
-                        width={400}
-                        height={400}
-                        alt="Ilustrasi karakter Megumine"
-                        className="w-full h-auto object-cover object-bottom"
-                    />
+
+                <div className="flex justify-start md:justify-center items-center w-60 py-6">
+                    <div className="w-50">
+                        <AspectRatio ratio={1}>
+                            <Image
+                                src="/images/home/elric.webp"
+                                alt="Elric"
+                                fill
+                                className="object-contain md:scale-x-100 scale-x-[-1]"
+                                priority
+                            />
+                        </AspectRatio>
+                    </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
