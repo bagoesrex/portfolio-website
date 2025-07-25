94b1f1af3b55f324467b1a88b3393c377c01ce0a
bagoesrex <bagoesrex@gmail.com>
2025-07-25 09:54:56 +0700

    feat(layout): add logo icon and update header & footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,13 +1,17 @@
 import SocialMedia from "../shared/social-media";
+import Image from "next/image";
 
 export default function Footer() {
     return (
         <footer className="px-2">
             <div className="flex flex-row justify-between max-w-3xl mx-auto px-3 py-10 h-full border-t-1 border-gray-500">
                 <div className="flex-2/4 flex flex-col gap-3">
-                    <h3 className="font-bold text-secondary line-clamp-2">
-                        Bagus Rahmawan
-                    </h3>
+                    <div className="flex flex-row gap-1 items-center">
+                        <Image src="/icons/logo.svg" alt="Logo" width={38} height={38} priority />
+                        <h3 className="font-bold text-secondary line-clamp-2">
+                            Bagus Rahmawan
+                        </h3>
+                    </div>
                     <p className="text-sm">
                         A tech enthusiast and web developer from Semarang, Indonesia, passionate about building meaningful digital experiences.
                     </p>
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Navbar from "./navbar";
+import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 w-full z-50 h-14 mt-1 px-2">
-            <div className="flex flex-row justify-between border-b-1 border-gray-500 items-center max-w-3xl mx-auto px-5 h-full">
+        <header className="fixed top-0 w-full z-50 h-14 px-2">
+            <div className="flex flex-row justify-between border-b-1 border-gray-500 bg-background/60 backdrop-blur-md items-center max-w-3xl mx-auto px-5 h-full">
                 <Link href={"/"}>
-                    <div>
-                        <h1>{`< Header Icon />`}</h1>
+                    <div className="flex flex-row gap-1 justify-center items-center text-primary">
+                        <Image src="/icons/logo.svg" alt="Logo" width={38} height={38} priority />
+                        <h1 className="font-extrabold">Bagoes Rex</h1>
                     </div>
                 </Link>
                 <Navbar />
