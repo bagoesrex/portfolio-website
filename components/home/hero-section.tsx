5afe2a8e3baafb485bc0e01244f01c7018652448
bagoesrex <bagoesrex@gmail.com>
2025-08-06 20:04:22 +0700

    feat(home): add CTA button link to About page in hero section

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import { AspectRatio } from "../ui/aspect-ratio";
+import { Button } from "../ui/button";
+import { User } from "lucide-react";
+import SocialMedia from "../shared/social-media";
+import Link from "next/link";
 
 export default function HeroSection() {
     return (
@@ -15,6 +19,17 @@ export default function HeroSection() {
                     <p className="leading-relaxed">
                         A tech enthusiast with a strong interest in building responsive and interactive web applications. I enjoy experimenting with new technologies and creating clean user interfaces.
                     </p>
+                    <div className="flex flex-col md:flex-row mt-2 items-start md:items-center gap-2 md:gap-3">
+                        <Link href="/about">
+                            <Button asChild className="text-sm">
+                                <span>
+                                    <User className="mr-2 h-4 w-4" />
+                                    More About Me
+                                </span>
+                            </Button>
+                        </Link>
+                        <SocialMedia />
+                    </div>
                 </div>
 
                 <div className="flex justify-start md:justify-center items-center w-60 py-1">
