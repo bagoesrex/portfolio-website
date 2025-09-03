98da66e6f594128bdfc5e06474e21c7d01b3c49d
bagoesrex <bagoesrex@gmail.com>
2025-09-04 02:16:37 +0700

    fix(blog): improve blog card UI and format date using dayjs

--- components/blog/blog-card.tsx
+++ components/blog/blog-card.tsx
@@ -1,7 +1,8 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Clock } from "lucide-react";
+import { Calendar } from "lucide-react";
 import { PortableText, PortableTextBlock } from "next-sanity";
 import Link from "next/link";
+import dayjs from "dayjs";
 
 interface BlogCardProps {
     slug: string;
@@ -12,17 +13,17 @@ interface BlogCardProps {
 
 export default function BlogCard({ slug, title, date, body }: BlogCardProps) {
     return (
-        <Card className="hover:shadow-md transition rounded-none bg-white/70 p-0">
+        <Card className="hover:shadow-md hover:-translate-y-0.5 transition-transform duration-300 rounded-md bg-white/70 p-0 group">
             <Link href={`/blog/${slug}`}>
                 <CardContent className="flex flex-col py-3 gap-3">
-                    <div className="flex flex-col gap-1">
-                        <h3 className="text-lg font-semibold text-secondary">{title}</h3>
+                    <div className="flex flex-col gap-2">
+                        <h3 className="text-lg font-semibold group-hover:text-secondary">{title}</h3>
                         <div className="flex flex-row items-center gap-2 text-foreground/70">
-                            <Clock size={12} />
-                            <p className="text-xs">{new Date(date).toLocaleDateString()}</p>
+                            <Calendar size={12} />
+                            <p className="text-xs">{dayjs(date).format("MMM DD, YYYY")}</p>
                         </div>
                     </div>
-                    <div className="text-sm mt-1 line-clamp-2">
+                    <div className="text-sm mt-1 line-clamp-3">
                         {Array.isArray(body) && <PortableText value={body} />}
                     </div>
                 </CardContent>
