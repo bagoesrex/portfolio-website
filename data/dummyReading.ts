8a712b4e45e9d4d87778f0a87abe5d4c70e1217d
bagoesrex <bagoesrex@gmail.com>
2025-09-11 14:10:45 +0700

    feat(about): add initial dummy reading data for activities section

---
+++ data/dummyReading.ts
@@ -0,0 +1,10 @@
+import { Reading } from "@/types/reading";
+
+export const dummyReading: Reading = {
+    title: "Chainsaw Man",
+    author: "Tatsuki Fujimoto",
+    coverUrl: "https://chainsawmann.com/wp-content/uploads/2023/11/Chainsaw_Man_Volume_11-649x1024.webp",
+    readUrl: "https://mangaplus.shueisha.co.jp/titles/100034",
+    totalPages: 200,
+    currentPage: 45,
+}
---
+++ types/reading.ts
@@ -0,0 +1,8 @@
+export interface Reading {
+    title: string
+    author: string
+    coverUrl: string
+    readUrl: string
+    totalPages: number
+    currentPage: number
+}
