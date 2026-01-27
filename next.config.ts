e212c0f1ff3587be7e192231a7e47932aedb9371
bagoesrex <bagoesrex@gmail.com>
2026-01-27 18:07:55 +0700

    chore: update next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.sanity.io", "i.scdn.co"],
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      }
+    ]
   },
 };
 
