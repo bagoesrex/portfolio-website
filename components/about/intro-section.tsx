ddbcbc14e7822ad085ed70d0f18a3d7d3ad27e49
bagoesrex <bagoesrex@gmail.com>
2026-02-05 10:36:26 +0700

    feat(about): add quote card to intro section

--- components/about/intro-section.tsx
+++ components/about/intro-section.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import MaxWidthWrapper from "../layout/max-width-wrapper";
 import SocialLinks from "../shared/social-links";
+import { FaQuoteRight } from "react-icons/fa";
 
 export default function IntroSection() {
   return (
     <section id="intro">
-      <MaxWidthWrapper className="space-y-10 pt-30">
+      <MaxWidthWrapper className="space-y-10 pt-30 pb-10">
         <div className="flex items-center gap-4">
           <div className="space-y-4">
             <h1 className="text-primary text-2xl font-extrabold">About Me</h1>
@@ -19,6 +20,11 @@ export default function IntroSection() {
           <div className="relative aspect-square size-55">
             <Image src="/images/about/me.webp" alt="Intro Image" fill priority className="rounded-full object-cover" />
           </div>
+        </div>
+        <div className="relative max-w-100 space-y-1.5 overflow-hidden rounded-[1.5px] border-[0.8px] border-gray-200 p-4">
+          <blockquote className="pr-12 text-sm font-light">To obtain something, something of equal value must be given</blockquote>
+          <p className="text-muted-foreground text-xs">- Equivalent Exchange</p>
+          <FaQuoteRight className="absolute -top-3 -right-1 size-13 -rotate-18 text-gray-200" />
         </div>
         <div className="space-y-2">
           <p className="font-semibold">Let&rsquo;s get in touch!</p>
