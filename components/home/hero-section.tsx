c9741e25c99cc79d054d04ef9709889d2b8445d4
bagoesrex <bagoesrex@gmail.com>
2026-02-05 22:14:41 +0700

    style(hero): improve hero section responsiveness

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -8,8 +8,8 @@ import { FaUser } from "react-icons/fa";
 export default function HeroSection() {
   return (
     <section id="hero">
-      <MaxWidthWrapper className="space-y-1.5 pt-30 pb-20">
-        <div className="flex items-center gap-10">
+      <MaxWidthWrapper className="space-y-6 pt-30 pb-20 md:space-y-1.5">
+        <div className="flex flex-col-reverse items-start gap-10 md:flex-row md:items-center">
           <div className="flex flex-col gap-3.5">
             <h1 className="text-primary text-3xl font-extrabold">Hi, I&apos;m Bagus</h1>
             <p className="font-sans leading-7.5">
@@ -17,12 +17,12 @@ export default function HeroSection() {
               technologies and creating clean user interfaces.
             </p>
           </div>
-          <div className="relative aspect-square size-55">
-            <Image src="/images/home/elric.webp" alt="Hero Image" fill priority className="object-contain" />
+          <div className="relative aspect-square size-45 md:size-55">
+            <Image src="/images/home/elric.webp" alt="Hero Image" fill priority className="scale-x-[-1] object-contain md:scale-x-100" />
           </div>
         </div>
-        <div className="flex gap-5">
-          <Button asChild className="px-4 py-5">
+        <div className="flex flex-col items-center gap-5 md:flex-row md:items-start">
+          <Button asChild className="w-full px-4 py-5 md:w-fit">
             <Link href="/about">
               <div className="flex items-center gap-3">
                 <FaUser />
