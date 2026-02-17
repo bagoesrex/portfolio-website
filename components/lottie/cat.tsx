2e693d3dee0e4dc2d20bd259ce096739c1a6f47c
bagoesrex <bagoesrex@gmail.com>
2026-02-17 19:36:10 +0700

    refactor(home): improve lottie cat animation in hero section

--- components/lottie/cat.tsx
+++ components/lottie/cat.tsx
@@ -3,5 +3,5 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Cat() {
-  return <DotLottieReact src="/lottie/cat-movement.lottie" loop autoplay />;
+  return <DotLottieReact src="/lottie/cat-movement.lottie" loop autoplay useFrameInterpolation={false} />;
 }
