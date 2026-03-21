37802bd17d92012acc287d7ae8dcca652ea3e078
bagoesrex <bagoesrex@gmail.com>
2026-03-21 20:30:50 +0700

    feat(home): update banner section and optimize image asset

--- components/home/banner-section.tsx
+++ components/home/banner-section.tsx
@@ -5,10 +5,19 @@ export default function BannerSection() {
   return (
     <section id="banner">
       <MaxWidthWrapper className="space-y-4 pb-10">
-        <div className="relative h-50 min-w-60 self-center md:h-90">
-          <Image src="/images/home/chomusuke.webp" alt="Banner Image" fill className="rounded-md object-cover drop-shadow-2xl md:drop-shadow-xl" />
+        <div className="flex justify-end">
+          <div className="w-full space-y-2 md:w-60">
+            <div className="relative h-50 w-full md:h-30 md:max-w-60 md:self-end">
+              <Image
+                src="/images/home/chomusuke.webp"
+                alt="Banner Image"
+                fill
+                className="rounded-md object-cover drop-shadow-2xl md:drop-shadow-xl"
+              />
+            </div>
+            <p className="text-center text-sm font-light text-gray-600 italic md:text-end">😽 Zzz 😽</p>
+          </div>
         </div>
-        <p className="text-center text-sm font-light text-gray-600 italic">My Chomusuke Image 😸</p>
       </MaxWidthWrapper>
     </section>
   );
