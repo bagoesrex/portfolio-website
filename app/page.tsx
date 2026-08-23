8e5bda0d8202001a4387bf148b65e0ce756efcf0
bagoesrex <bagoesrex@gmail.com>
2026-08-23 20:58:52 +0700

    feat: build portfolio hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import HeroSection from "@/components/home/HeroSection";
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="mt-10 text-center text-4xl font-bold">
-        Welcome to My Portfolio
-      </h1>
-    </div>
-  );
+  return <HeroSection />;
 }
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,8 +3,21 @@ import MaxWidthWrapper from "../layout/MaxWidthWrapper";
 export default function HeroSection() {
   return (
     <section id="hero">
-      <MaxWidthWrapper className="space-y-1.5 pt-30 pb-20">
-        <h1></h1>
+      <MaxWidthWrapper className="space-y-1.5 pt-35 pb-20">
+        <div className="flex justify-between gap-15">
+          <div className="space-y-5">
+            <div className="space-y-1">
+              <h1 className="font-mono text-2xl font-bold">Bagus Rahmawan</h1>
+              <p>Tech Enthusiast</p>
+            </div>
+            <p>
+              A tech enthusiast with a strong interest in building responsive
+              and interactive web applications. I enjoy experimenting with new
+              technologies and creating clean user interfaces.
+            </p>
+          </div>
+          <div className="min-h-full min-w-40 bg-red-300"></div>
+        </div>
       </MaxWidthWrapper>
     </section>
   );
